e1cd1c2fdea1767a36f0f5cf6301f8c3c25ec76b
ogawahideo <ogawahideo@ogawahideo-no-MacBook-Pro.local>
2018-10-10 18:06:53 +0900

    add zeroset🤔

--- scripts/ma24126a_ros.py
+++ scripts/ma24126a_ros.py
@@ -18,16 +18,24 @@ class ma24126a_controller(object):
         self.pm = pymeasure.usbpm.open("ma24126a")
         self.rate = rospy.get_param("~rate")
 
+        self.zero_set_flag =0
         self.power_flag = 0
         self.capt_flag = 0
-        self.ave_flag = 0
+        self.avemode_flag = 0
 
+        self.sub_zeroset = rospy.Subscriber("topic_sub_zeroset", Float64, self.zero_set_switch)
         self.pub_power = rospy.Publisher("topic_pub_power", Float64, queue_size = 1)
         self.sub_power = rospy.Subscriber("topic_sub_power", Float64, self.power_switch)
-        self.sub_change_avemode = rospy.Subscriber("topic_sub_change_avemode", Float64, self.ave_switch)
+        self.sub_change_avemode = rospy.Subscriber("topic_sub_change_avemode", Float64, self.avemode_switch)
         self.pub_change_avemode = rospy.Publisher("topic_pub_change_avemode", Float64, queue_size = 1)
         self.sub_change_capt = rospy.Subscriber("topic_sub_change_capt", Float64, self.capt_switch)
         self.pub_change_capt = rospy.Publisher("topic_pub_change_capt", Float64, queue_size = 1)
+
+#flag
+
+    def zero_set_switch(self,q):
+        self.zero_set_flag = q.data
+        return
 
     def power_switch(self,q):
         self.power_flag = q.data
@@ -38,10 +46,22 @@ class ma24126a_controller(object):
         self.capt_flag = 1
         return
 
-    def ave_switch(self,q):
+    def avemode_switch(self,q):
         self.ave = q.data
-        self.ave_flag = 1
+        self.avemode_flag = 1
         return
+
+#main methond
+    def zero_set(self):
+        while not rospy.is_shutdown():
+            if self.zero_set_flag == 0:
+                time.sleep(self.rate)
+                continue
+
+            self.pm.zero_set()
+
+            self.zero_set_flag = 0
+            continue
 
     def power(self):
         while not rospy.is_shutdown():
@@ -73,7 +93,7 @@ class ma24126a_controller(object):
 
     def change_avemode(self):
         while not rospy.is_shutdown():
-            if self.ave_flag == 0:
+            if self.avemode_flag == 0:
                 time.sleep(self.rate)
                 continue
 
@@ -83,7 +103,7 @@ class ma24126a_controller(object):
             msg.data = float(self.pm.check_avemode())
             self.pub_change_avemode.publish(msg)
 
-            self.ave_flag = 0
+            self.avemode_flag = 0
             continue
 
     def start_thread(self):
@@ -96,6 +116,9 @@ class ma24126a_controller(object):
         th3 = threading.Thread(target=self.change_avemode)
         th3.setDaemon(True)
         th3.start()
+        th4 = threading.Thread(target=self.zero_set)
+        th4.setDaemon(True)
+        th4.start()
 
 if __name__ == "__main__" :
     rospy.init_node("ma24126a")
