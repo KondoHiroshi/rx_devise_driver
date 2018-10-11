1d79eaf252ea8274ca214f0b379ac11915bce04d
ogawahideo <ogawahideo@ogawahideo-no-MacBook-Pro.local>
2018-10-11 12:01:16 +0900

    loooop

--- scripts/ma24126a_ros.py
+++ scripts/ma24126a_ros.py
@@ -76,7 +76,6 @@ class ma24126a_controller(object):
                 continue
 
             if self.power_flag == 1:
-                while True:
                     msg = Float64()
                     msg.data = float(self.pm.quary(b"PWR?\n"))
                     self.pub_power.publish(msg)
