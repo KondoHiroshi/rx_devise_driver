2ec58ce00c68fc7ebe2b578222ff5b45c907ec7d
ogawahideo <ogawahideo@MacBook-Air.local>
2018-10-25 18:37:14 +0900

    commentout

--- scripts/ml2437a.py
+++ scripts/ml2437a.py
@@ -16,8 +16,7 @@ class ml2437a_driver(object):
         self.IP = IP
         self.GPIB = GPIB
         self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
-        self.com.send('CHUNIT %d, DBM' %(ch))
-        self.com.send('CHRES %d, %d' %(ch, resolution))
+
 
     def measure(self, ch=1, resolution=3):
         self.com.open()
@@ -28,6 +27,8 @@ class ml2437a_driver(object):
         power = float(ret)
         return power
 """
+        self.com.send('CHUNIT %d, DBM' %(ch))
+        self.com.send('CHRES %d, %d' %(ch, resolution))
     def set_ave(self, ch=1, mode, ave_num):
         self.com.open()
         self.com.send("AVE A, %s, %d" %(mode, ave_num))
