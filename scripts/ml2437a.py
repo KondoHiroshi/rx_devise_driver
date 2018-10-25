22c8f4c8be700e6897fd80b27a076d7f649c4b16
ogawahideo <ogawahideo@MacBook-Air.local>
2018-10-25 18:21:36 +0900

    send change

--- scripts/ml2437a.py
+++ scripts/ml2437a.py
@@ -16,11 +16,11 @@ class ml2437a_driver(object):
         self.IP = IP
         self.GPIB = GPIB
         self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
+        self.com.send('CHUNIT %d, DBM' %(ch))
+        self.com.send('CHRES %d, %d' %(ch, resolution))
 
     def measure(self, ch=1, resolution=3):
         self.com.open()
-        self.com.send('CHUNIT %d, DBM' %(ch))
-        self.com.send('CHRES %d, %d' %(ch, resolution))
         self.com.send('o %d' %(ch))
         time.sleep(0.1)
         ret = self.com.readline()
