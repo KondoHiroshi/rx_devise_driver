3c3b33e50bbac3231cb8fab6dd9990b54b4f4fc7
ogawahideo <ogawahideo@MacBook-Air.local>
2018-11-02 15:30:57 +0900

    a

--- scripts/ml2437a_ros.py
+++ scripts/ml2437a_ros.py
@@ -27,16 +27,17 @@ class ml2437a_controller(object):
         self.sub_ave_count = rospy.Subscriber("topic_sub_ave_count", Int32, self.ave_count)
 
     def power(self,q):
+        print("ABC")
         while not rospy.is_shutdown:
             print("aaa")
             while q.data == 0 :
                 print("a")
                 continue
             while q.data == 1 :
-                ret = pm.measure()
+                ret = self.pm.measure()
                 msg = Float64()
                 msg.data = float(ret)
-                pub_power.publish(msg)
+                self.pub_power.publish(msg)
                 continue
             continue
 
