6a7d9c2b0984cde0bc0e269db3e2ef5e16c818ff
ogawahideo <ogawahideo@MacBook-Air.local>
2018-11-13 12:54:16 +0900

    add monitor

--- scripts/ml2437a_ros.py
+++ scripts/ml2437a_ros.py
@@ -156,6 +156,26 @@ if __name__ == "__main__" :
     rospy.init_node("ml2437a")
     ctrl = ml2437a_controller()
     rospy.spin()
+    rate = rospy.get_param('~rate')
+    rsw_id = rospy.get_param('~rsw_id')
+
+    mode = 'diff'
+    ch = 10
+    pub = rospy.Publisher('{0}_rsw{1}_{2}{3}'.format(node_name, rsw_id, mode, ch), Float64, queue_size=1)
+
+    try:
+        ad = pyinterface.open(3177, rsw_id)
+    except OSError as e:
+        rospy.logerr("{e.strerror}. node={node_name}, rsw={rsw_id}".format(**locals()))
+        sys.exit()
+
+while not rospy.is_shutdown():
+    msg = Float64()
+    ret = ad.input_voltage(ch, mode)
+    msg.data = ret
+    pub.publish(msg)
+    time.sleep(rate)
+    continue
 
 #2018/11/01
 #written by H.Kondo
